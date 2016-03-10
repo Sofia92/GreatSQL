53a92aeed8061cba79aa4e05a5620c48c09f7b7e
Jonas <gaopinsong@live.cn>
2016-03-11 00:30:46 +0800

    pre SQLServer

---
+++ dotNet/GreatSQL/GreatSQL.SQLServer/SqlExecuter.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.SqlClient;
+using GreatSQL.Models;
+using GreatSQL.Models.Interfaces;
+
+namespace GreatSQL.SQLServer
+{
+    public class SqlExecuter : ISqlExecuter
+    {
+        public int Execute(SqlItem sql)
+        {
+            using (var conn = new SqlConnection(""))
+            {
+                conn.Open();
+                using (var command = new SqlCommand(sql.Body, conn))
+                {
+                    return command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
