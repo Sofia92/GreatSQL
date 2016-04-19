e8d67f7f8c6a39efcea0ae895b3f987cc2ed320c
Jonas <gaopinsong@live.cn>
2016-04-20 01:31:25 +0800

    init front

--- dotNet/GreatSQL/GreatSQL.Front/app.js
+++ dotNet/GreatSQL/GreatSQL.Front/app.js
@@ -1,1 +1,13 @@
-﻿console.log('Hello world');
+﻿var Express = require("express");
+var App = Express();
+
+App.use(Express.static("site"));
+
+var Server = App.listen(3000, function () {
+    var host = Server.address().address;
+    var port = Server.address().port;
+   
+    console.log("Example app listening at http://%s:%s", host, port);
+
+    console.log("Start !");
+});
